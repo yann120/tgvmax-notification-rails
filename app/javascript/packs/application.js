15b8458240c7cd82cd0268b6d1eccd886eb00244
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-10 18:06:12 +0000

    add burger javascript for navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,15 @@ require('easy-autocomplete')
 window.jQuery = $;
 window.$ = $;
 require("packs/form")
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
