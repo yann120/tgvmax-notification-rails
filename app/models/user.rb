68a7b6dc1571bc931358ec12cda68aff2c3c4a4c
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-11 21:57:42 +0000

    Fix User creation bug on Model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :tgvmax_key, format: { with: /HC[0-9]{9}/, message: ' - Enter a correct TGVMAX key like HC123456789' }
-  validate :validate_age
+  validates :tgvmax_key,
+            format: { with: /HC[0-9]{9}/, message: ' - Enter a correct TGVMAX key like HC123456789' },
+            on: :update
+  validate :validate_age, on: :update
 
   def tgvmax_key_valid?
     tgvmax_key&.match(/^HC[0-9]{9}$/)
@@ -15,6 +15,6 @@ class User < ApplicationRecord
   def validate_age
     return if birthdate.present? && birthdate < 16.years.ago && birthdate > 28.years.ago
 
-    errors.add(:birthdate, ': You should be between 16 and 27 years old')
+    errors.add(:birthdate, ': You should be between 16 and 27 years old') 
   end
 end
